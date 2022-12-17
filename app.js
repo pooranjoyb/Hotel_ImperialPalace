1cd3a953df0da2c5b3782172f2bbafa7894913c0
pooranjoyb <pooranjoyb2016@gmail.com>
2022-12-17 14:00:55 +0530

    Serialized form data into json

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navbar.classList.toggle("active");
 })
+
+$(document).ready(function () {
+    $("#btnSubmit").click(function (e) {
+        var jsonData = {};
+
+        var formData = $("#myform").serializeArray();
+
+        $.each(formData, function () {
+            if (jsonData[this.name]) {
+                if (!jsonData[this.name].push) {
+                    jsonData[this.name] = [jsonData[this.name]];
+                }
+                jsonData[this.name].push(this.value || '');
+            } else {
+                jsonData[this.name] = this.value || '';
+            }
+
+
+        });
+        let obj = JSON.stringify(jsonData, null, 2);
+        console.log(obj);
+        e.preventDefault();
+    });
+});
